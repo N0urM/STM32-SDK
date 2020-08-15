e3ffa001947335643fa5fb9bf8f76939eb17d947
nour mansour <pop.princess_98@hotmail.com>
2020-08-15 21:32:53 +0200

    Registers address fix

--- 02-MCAL/02-GPIO/GPIO_interface.h
+++ 02-MCAL/02-GPIO/GPIO_interface.h
@@ -7,7 +7,7 @@
 #ifndef     GPIO_INTERFACE_H
 #define     GPIO_INTERFACE_H
 
-/*********** Pulbic data types *************/
+/*********** Public data types *************/
 typedef enum {
     PORTA,
     PORTB,
@@ -15,22 +15,22 @@ typedef enum {
 }t_PORT;
 
 typedef enum{
-    PIN0,
-    PIN1,
-    PIN2,
-    PIN3,
-    PIN4,
-    PIN5,
-    PIN6,
-    PIN7,
-    PIN8,
-    PIN9,
-    PIN10,
-    PIN11,
-    PIN12,
-    PIN13,
-    PIN14,
-    PIN15,
+    PIN0 = 0,
+    PIN1 = 1,
+    PIN2 = 2,
+    PIN3 = 3,
+    PIN4 = 4,
+    PIN5 = 5,
+    PIN6 = 6,
+    PIN7 = 7,
+    PIN8 = 8,
+    PIN9 = 9,
+    PIN10 = 10,
+    PIN11 = 11,
+    PIN12 = 12,
+    PIN13 = 13,
+    PIN14 = 14,
+    PIN15 = 15,
 }t_PIN;
 
 /******************* public constants *************************/
@@ -72,7 +72,7 @@ void GPIO_voidInitPortPinDirection(t_PORT cpy_port , t_PIN cpy_pin  ,u8 mode);
 
 /*
     Function Name: GPIO_voidInitPortDirection
-    Description  : initializa a GPIO port direction and mode
+    Description  : initialize a GPIO port direction and mode
     Parameters   :  
         port: PORTA,PORTB,PORTC
         mode: input/output definitions 
