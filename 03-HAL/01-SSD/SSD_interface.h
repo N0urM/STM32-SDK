656bfc33144ba55e93651ae7de1a68fd603cc6f5
nour mansour <pop.princess_98@hotmail.com>
2020-08-16 22:04:59 +0200

    SSD v2.0 MUX_ADDED

--- 03-HAL/01-SSD/SSD_interface.h
+++ 03-HAL/01-SSD/SSD_interface.h
@@ -1,32 +1,61 @@
 /*******************************************************/ 
 /* Author: Nourhan Mansour                             */
-/* Date  : 14/8/2020                                   */
-/* Vesion: 1.0                                         */
+/* Date  : 16/8/2020                                   */
+/* Version: 2.0                                        */
 /* File  : SSD_interface.h                             */
 /*******************************************************/ 
 #ifndef     SSD_INTERFACE_H
 #define     SSD_INTERFACE_H
 
-/******************* public Constants *************************/
 
+/************************ public dataTypes *************************/
 
-/******************* public dataTypes *************************/
-typedef enum{
-    COMMON_ANODE,
-    COMMON_CATHODE,
-}t_SSD_TYPE;
-
+/*
+ * Define Wiring Scheme for SSD
+ * Warning: Pins must be continuous on the same port
+ */
 typedef struct
 {
-    t_SSD_TYPE Type;
     t_PORT port; 
     t_PIN start_pin;
     t_PIN end_pin; 
-    t_PIN Control_pin;
+
 }t_SSD;
 
-/******************* public functions *************************/
-void SSD_voidInit(t_SSD cpy_SSD);
-void SSD_voidDisplay (t_SSD cpy_SSD , u8 cpy_digit);
 
-#endif
+/*********************** public functions *************************/
+
+/*
+ *  Function: SSD_voidInit
+ *  Parameters: Struct of the wiring scheme
+ *  Description: Initialize GPIO pins and Clock
+ *  No Return
+ *
+ */
+
+void SSD_voidInit(t_SSD cpy_SSD);
+
+/*
+ *  Function: SSD_voidUpdateDisplay
+ *  Parameters: Struct of the wiring scheme
+ *  Description: Does the multiplexing between one or more SSDs
+ *  			 MAX : 4 SSDs
+ *  No Return
+ *
+ */
+void SSD_voidUpdateDisplay (t_SSD cpy_SSD);
+
+/*
+ *  Function: SSD_voidSetSSDDigit
+ *  Parameters: SSD_IDX: index of the SSD to assign a digit 0~3
+ *  			digit  : digit to display on the selected SSD
+ *  Description: Stores a digit to display
+ *  No Return
+ *
+ */
+
+void SSD_voidSetSSDDigit(u8 SSD_IDX , u8 digit);
+
+/***************************************************************/
+
+#endif // End Of File
--- 03-HAL/01-SSD/SSD_program.c
+++ 03-HAL/01-SSD/SSD_program.c
@@ -1,7 +1,7 @@
 /*******************************************************/ 
 /* Author: Nourhan Mansour                             */
-/* Date  : 14/8/2020                                   */
-/* Vesion: 1.0                                         */
+/* Date  : 16/8/2020                                   */
+/* Vesion: 2.0                                         */
 /* File  : SSD_program.c                               */
 /*******************************************************/ 
 
@@ -19,49 +19,98 @@
 #include "SSD_config.h"
 
 void SSD_voidInit(t_SSD cpy_SSD){
-    switch (cpy_SSD.port)
-    {
-    case PORTA:
-        RCC_voidEnableClock(t_APB2, RCC_APB2ENR_IOPAEN);
-        break;
-    case PORTB:
-        RCC_voidEnableClock(t_APB2, RCC_APB2ENR_IOPBEN);
-        break;
-    case PORTC:
-        RCC_voidEnableClock(t_APB2, RCC_APB2ENR_IOPCEN);
-        break;
-    default:
-        break;
-    }
 
-    u8 i = 0;
-    GPIO_voidInitPortPinDirection(cpy_SSD.port , cpy_SSD.Control_pin , OUTPUT_PUSH_PULL_10MHZ);
-    for(i=cpy_SSD.start_pin ; i <= cpy_SSD.end_pin ; i++)
-    {
-        GPIO_voidInitPortPinDirection(cpy_SSD.port, i , OUTPUT_PUSH_PULL_10MHZ);
-    }
+		// Enable Clock for Data pins
+    	switch (cpy_SSD.port)
+		{
+		case PORTA:
+			RCC_voidEnableClock(t_APB2, RCC_APB2ENR_IOPAEN);
+			break;
+		case PORTB:
+			RCC_voidEnableClock(t_APB2, RCC_APB2ENR_IOPBEN);
+			break;
+		case PORTC:
+			RCC_voidEnableClock(t_APB2, RCC_APB2ENR_IOPCEN);
+			break;
+		default:
+			break;
+		}
+
+        // Enable Clock for Control pins
+    	#if SSD_SELECTION_PORT == PORTA
+        	RCC_voidEnableClock(t_APB2, RCC_APB2ENR_IOPAEN);
+        #elif SSD_SELECTION_PORT == PORTB
+        	RCC_voidEnableClock(t_APB2, RCC_APB2ENR_IOPBEN);
+        #elif SSD_SELECTION_PORT == PORTC
+        	RCC_voidEnableClock(t_APB2, RCC_APB2ENR_IOPCEN);
+        #endif
+
+        // initialize O/P configuration for control pins
+    	u8 i;
+    	for(i=0 ; i< MUX_SSD_NUM ; i++){
+    		GPIO_voidInitPortPinDirection(SSD_SELECTION_PORT , SSID_CONTROL_PINS[i]  , OUTPUT_PUSH_PULL_10MHZ);
+    	}
+
+    	// initialize O/P configuration for data pins
+		for(i=cpy_SSD.start_pin ; i <= cpy_SSD.end_pin ; i++)
+		{
+			GPIO_voidInitPortPinDirection(cpy_SSD.port, i , OUTPUT_PUSH_PULL_10MHZ);
+		}
 }
 
-void SSD_voidDisplay(t_SSD cpy_SSD , u8 cpy_digit){
 
-    u8 toDisplay = number[cpy_digit];
-    switch (cpy_SSD.Type)
-    {
-    case COMMON_ANODE:
-        toDisplay = ~ toDisplay ;
-        GPIO_voidSetPortPinValue(cpy_SSD.port, cpy_SSD.Control_pin , 1 );
-        break;
-    case COMMON_CATHODE:
-        GPIO_voidSetPortPinValue(cpy_SSD.port, cpy_SSD.Control_pin , 0 );
-        break;
-    default:
-        break;
-    }
-    u8 i = 0 , j=0;
-    for(i=cpy_SSD.start_pin ; i <= cpy_SSD.end_pin ; i++)
-    {
-        GPIO_voidSetPortPinValue(cpy_SSD.port, i , (toDisplay >> j) & 0x01 );
-        j++;
-    }
+void SSD_voidUpdateDisplay (t_SSD cpy_SSD)
+{
+	// Time Slice for each SSD
+	u8 i =0;
+	while(i < MUX_SSD_NUM)
+	{
+		u8 toDisplay = number[SSD_digit[i]];
+		// Neutralize control pins on all SSDs
+		#if COMMON_ANODE_CATHODE == 1
+			toDisplay = ~ toDisplay ;
+			u8 j = 0;
+			while(j<MUX_SSD_NUM){
+				GPIO_voidSetPortPinValue(SSD_SELECTION_PORT, SSID_CONTROL_PINS[j] , 0 );
+				j++;
+			}
 
+		#else
+			u8 j = 0 ;
+			while(j< MUX_SSD_NUM){
+				GPIO_voidSetPortPinValue(SSD_SELECTION_PORT, SSID_CONTROL_PINS[j] , 1 );
+				j++;
+			}
+
+		#endif
+
+		// Send data on BUS
+		u8 cnt = 0 , shift=0;
+		for(cnt = cpy_SSD.start_pin ; cnt <= cpy_SSD.end_pin ; cnt++)
+		{
+			GPIO_voidSetPortPinValue(cpy_SSD.port, cnt , (toDisplay >> shift) & 0x01 );
+			shift++;
+		}
+		#if COMMON_ANODE_CATHODE == 1
+			GPIO_voidSetPortPinValue(SSD_SELECTION_PORT, SSID_CONTROL_PINS[i] , 1 );
+		#else
+			GPIO_voidSetPortPinValue(SSD_SELECTION_PORT, SSID_CONTROL_PINS[i] , 0 );
+		#endif
+
+		delay_ms(2);
+		i++;
+	}
+}
+
+void SSD_voidSetSSDDigit(u8 SSID_IDX , u8 digit){
+	SSD_digit[SSID_IDX] = digit;
+}
+
+void delay_ms(u32 ms)
+{
+ u32 i;
+
+ for(i = 0; i < (SystemCoreClock/(12000)) * ms; i++){
+  __asm ("NOP");
+ }
 }
