509e9a6dcbc622dd0e0b9909809909b5a8add924
nour mansour <pop.princess_98@hotmail.com>
2020-08-21 11:18:54 +0200

    LED init Optimization

--- 03-HAL/02-LED/LED_program.c
+++ 03-HAL/02-LED/LED_program.c
@@ -18,61 +18,34 @@
 #include "LED_private.h"
 #include "LED_config.h"
 
-void LED_voidInitLED(t_PORT cpy_port , t_PIN cpy_pin  , LED_FREQ cpy_freq){
+void LED_voidInitLED(t_PORT cpy_port, t_PIN cpy_pin, LED_FREQ cpy_freq)
+{
     switch (cpy_port)
     {
     case PORTA:
-        RCC_voidEnableClock(t_APB2 , RCC_APB2ENR_IOPAEN);
-        switch (cpy_freq)
-        {
-        case LED_FREQ_2MHZ:
-            GPIO_voidInitPortPinDirection(PORTA , cpy_pin ,  OUTPUT_PUSH_PULL_2MHZ);    
-            break;
-        case LED_FREQ_10MHZ:
-            GPIO_voidInitPortPinDirection(PORTA , cpy_pin ,  OUTPUT_PUSH_PULL_10MHZ);    
-            break;
-        case LED_FREQ_50MHZ:
-            GPIO_voidInitPortPinDirection(PORTA , cpy_pin ,  OUTPUT_PUSH_PULL_50MHZ);    
-            break;
-        default:
-            break;
-        }
+        RCC_voidEnableClock(t_APB2, RCC_APB2ENR_IOPAEN);
         break;
     case PORTB:
-        RCC_voidEnableClock(t_APB2 , RCC_APB2ENR_IOPBEN);
-        switch (cpy_freq)
-        {
-        case LED_FREQ_2MHZ:
-            GPIO_voidInitPortPinDirection(PORTB , cpy_pin ,  OUTPUT_PUSH_PULL_2MHZ);    
-            break;
-        case LED_FREQ_10MHZ:
-            GPIO_voidInitPortPinDirection(PORTB , cpy_pin ,  OUTPUT_PUSH_PULL_10MHZ);    
-            break;
-        case LED_FREQ_50MHZ:
-            GPIO_voidInitPortPinDirection(PORTB , cpy_pin ,  OUTPUT_PUSH_PULL_50MHZ);    
-            break;
-        default:
-            break;
-        }
+        RCC_voidEnableClock(t_APB2, RCC_APB2ENR_IOPBEN);
         break;
     case PORTC:
-        RCC_voidEnableClock(t_APB2 , RCC_APB2ENR_IOPCEN);
-        switch (cpy_freq)
-        {
-        case LED_FREQ_2MHZ:
-            GPIO_voidInitPortPinDirection(PORTC , cpy_pin ,  OUTPUT_PUSH_PULL_2MHZ);    
-            break;
-        case LED_FREQ_10MHZ:
-            GPIO_voidInitPortPinDirection(PORTC , cpy_pin ,  OUTPUT_PUSH_PULL_10MHZ);    
-            break;
-        case LED_FREQ_50MHZ:
-            GPIO_voidInitPortPinDirection(PORTC , cpy_pin ,  OUTPUT_PUSH_PULL_50MHZ);    
-            break;
-        default:
-            break;
-        }
+        RCC_voidEnableClock(t_APB2, RCC_APB2ENR_IOPCEN);
         break;
-    
+    default:
+        break;
+    }
+
+    switch (cpy_freq)
+    {
+    case LED_FREQ_2MHZ:
+        GPIO_voidInitPortPinDirection(cpy_port, cpy_pin, OUTPUT_PUSH_PULL_2MHZ);
+        break;
+    case LED_FREQ_10MHZ:
+        GPIO_voidInitPortPinDirection(cpy_port, cpy_pin, OUTPUT_PUSH_PULL_10MHZ);
+        break;
+    case LED_FREQ_50MHZ:
+        GPIO_voidInitPortPinDirection(cpy_port, cpy_pin, OUTPUT_PUSH_PULL_50MHZ);
+        break;
     default:
         break;
     }
@@ -85,6 +58,7 @@ void LED_voidTurnOn(t_PORT cpy_port , t_PIN cpy_pin ){
         GPIO_voidSetPortPinValue(cpy_port , cpy_pin , 0);
     #endif 
 }
+
 void LED_voidTurnOff(t_PORT cpy_port , t_PIN cpy_pin ){
     #if LED_LOW == 0
         GPIO_voidSetPortPinValue(cpy_port , cpy_pin , 0);
@@ -92,6 +66,7 @@ void LED_voidTurnOff(t_PORT cpy_port , t_PIN cpy_pin ){
         GPIO_voidSetPortPinValue(cpy_port , cpy_pin , 1);
     #endif
 }
+
 void LED_voidToggle(t_PORT cpy_port , t_PIN cpy_pin ){
      if ( GPIO_u8GetPortPinValue(cpy_port , cpy_pin) == 1){
         GPIO_voidSetPortPinValue(cpy_port , cpy_pin , 0);
