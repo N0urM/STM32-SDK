e6954a98550d9319a28e82d0c6dab4a0db5055d5
nour mansour <pop.princess_98@hotmail.com>
2020-08-26 21:04:19 +0200

    GPIO add volatile keyword

--- 02-MCAL/02-GPIO/GPIO_private.h
+++ 02-MCAL/02-GPIO/GPIO_private.h
@@ -11,31 +11,31 @@
 /*************** Registers definitions ****************/
 
 /*********************** PORT A **********************/
-#define GPIOA_CRL           *((u32 *) 0x40010800)
-#define GPIOA_CRH           *((u32 *) 0x40010804)
-#define GPIOA_IDR           *((u32 *) 0x40010808)
-#define GPIOA_ODR           *((u32 *) 0x4001080C)
-#define GPIOA_BSRR          *((u32 *) 0x40010810)
-#define GPIOA_BRR           *((u32 *) 0x40010814)
-#define GPIOA_LCKR          *((u32 *) 0x40010818)
+#define GPIOA_CRL           *((volatile u32 *) 0x40010800)
+#define GPIOA_CRH           *((volatile u32 *) 0x40010804)
+#define GPIOA_IDR           *((volatile u32 *) 0x40010808)
+#define GPIOA_ODR           *((volatile u32 *) 0x4001080C)
+#define GPIOA_BSRR          *((volatile u32 *) 0x40010810)
+#define GPIOA_BRR           *((volatile u32 *) 0x40010814)
+#define GPIOA_LCKR          *((volatile u32 *) 0x40010818)
 
 /*********************** PORT B **********************/
-#define GPIOB_CRL           *((u32 *) 0x40010C00)
-#define GPIOB_CRH           *((u32 *) 0x40010C04)
-#define GPIOB_IDR           *((u32 *) 0x40010C08)
-#define GPIOB_ODR           *((u32 *) 0x40010C0C)
-#define GPIOB_BSRR          *((u32 *) 0x40010C10)
-#define GPIOB_BRR           *((u32 *) 0x40010C14)
-#define GPIOB_LCKR          *((u32 *) 0x40010C18)
+#define GPIOB_CRL           *((volatile u32 *) 0x40010C00)
+#define GPIOB_CRH           *((volatile u32 *) 0x40010C04)
+#define GPIOB_IDR           *((volatile u32 *) 0x40010C08)
+#define GPIOB_ODR           *((volatile u32 *) 0x40010C0C)
+#define GPIOB_BSRR          *((volatile u32 *) 0x40010C10)
+#define GPIOB_BRR           *((volatile u32 *) 0x40010C14)
+#define GPIOB_LCKR          *((volatile u32 *) 0x40010C18)
 
 /*********************** PORT C **********************/
-#define GPIOC_CRL           *((u32 *) 0x40011000)
-#define GPIOC_CRH           *((u32 *) 0x40011004)
-#define GPIOC_IDR           *((u32 *) 0x40011008)
-#define GPIOC_ODR           *((u32 *) 0x4001100C)
-#define GPIOC_BSRR          *((u32 *) 0x40011010)
-#define GPIOC_BRR           *((u32 *) 0x40011014)
-#define GPIOC_LCKR          *((u32 *) 0x40011018)
+#define GPIOC_CRL           *((volatile u32 *) 0x40011000)
+#define GPIOC_CRH           *((volatile u32 *) 0x40011004)
+#define GPIOC_IDR           *((volatile u32 *) 0x40011008)
+#define GPIOC_ODR           *((volatile u32 *) 0x4001100C)
+#define GPIOC_BSRR          *((volatile u32 *) 0x40011010)
+#define GPIOC_BRR           *((volatile u32 *) 0x40011014)
+#define GPIOC_LCKR          *((volatile u32 *) 0x40011018)
 
 /****************** AFIO Registers *******************/
 typedef struct 
