697fec8b3db41b74e0f377b8594c9b058255c648
nour mansour <pop.princess_98@hotmail.com>
2020-09-23 20:18:50 +0200

    STP V1.0

---
+++ 03-HAL/05- Serial To Parallel/STP_private.h
@@ -0,0 +1,11 @@
+/*******************************************************/ 
+/* Author : Nourhan Mansour                            */
+/* Date   : 14/9/2020                                  */
+/* Version: 1.0                                        */
+/* File   : STP_private.h                              */
+/*******************************************************/ 
+#ifndef     STP_PRIVATE_H
+#define     STP_PRIVATE_H
+
+
+#endif //End Of File
