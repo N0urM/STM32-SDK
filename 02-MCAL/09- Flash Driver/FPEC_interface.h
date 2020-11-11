96e7b3f65b4eb4f5537396d58590139ac0c9adf6
nour m <pop.princess_98@hotmail.com>
2020-11-11 20:09:39 +0200

    FPEC V2.0

--- 02-MCAL/09- Flash Driver/FPEC_interface.h
+++ 02-MCAL/09- Flash Driver/FPEC_interface.h
@@ -1,11 +1,17 @@
 /********************************************************/ 
 /* Author : Nourhan Mansour                             */
-/* Date   : 13/10/2020                                  */
-/* Version: 1.0                                         */
+/* Date   : 11/11/2020                                  */
+/* Version: 2.0                                         */
 /* File   : FPEC_interface.h                            */
 /********************************************************/ 
 #ifndef FPEC_INTERFACE_H
 #define FPEC_INTERFACE_H
+
+
+/******************** Public Constants **********************/
+#define WRITE_PROTECTION_ERROR      0
+#define PROGRAMMING_ERROR           1
+#define SUCCESSFULLL_OPERATION      2
 
 /****************** Functions prototypes ********************/
 
@@ -16,6 +22,38 @@
     Return value : none
 ***************************************************************/
 void FPEC_voidInit();
+
+/*************************************************************
+    Function Name: FPEC_UnlockOptionBytes
+    Description  : Perform unlock sequence for option bytes
+    Parameters   : none
+    Return value : none
+***************************************************************/
+void FPEC_UnlockOptionBytes();
+
+/*************************************************************
+    Function Name: FPEC_voidLock
+    Description  : Disable Flash Write Access 
+    Parameters   : none
+    Return value : none
+***************************************************************/
+void FPEC_voidLock();
+
+/*************************************************************
+    Function Name: FPEC_voidLockOptionBytes
+    Description  : Disable Flash Write Access for option Bytes
+    Parameters   : none
+    Return value : none
+***************************************************************/
+void FPEC_voidLockOptionBytes();
+
+/*************************************************************
+    Function Name: FPEC_u32ReadData
+    Description  : Read data from address cpy_address 
+    Parameters   : cpy_address: Data ddress in Litte Indian 
+    Return value : Word stored in specified address
+***************************************************************/
+u16 FPEC_u32ReadData(u16 * cpy_address);
 
 /*************************************************************
     Function Name: FPEC_voidWriteData
@@ -35,7 +73,7 @@ void FPEC_voidWriteData ( u16 * cpy_address , u16 * cpy_data , u8 cpy_length);
                              Max No of pages in stm10x = 64
     Return value : none
 ***************************************************************/
-void FPEC_voidEraseDataPage (u32 cpy_page);
+void FPEC_voidEraseDataPage (u8 cpy_Page);
 
 /*************************************************************
     Function Name: FPEC_voidEraseDataPage
@@ -43,9 +81,9 @@ void FPEC_voidEraseDataPage (u32 cpy_page);
     Parameters   : cpy_startPage: Start pageNo to erase 
                    cpy_endPage  : Final pageNo to erase        
     Return value : none
-    Notes        : •Page size = 1k 
-                   •Max No of pages in stm10x = 64
-                   •Start page must be less than end page
+    Notes        : Page size = 1k
+                   Max No of pages in stm10x = 64
+                   Start page must be less than end page
 ***************************************************************/
 void FPEC_voidEraseRange(u8 cpy_startPage , u8 cpy_endPage);
 
@@ -56,5 +94,17 @@ void FPEC_voidEraseRange(u8 cpy_startPage , u8 cpy_endPage);
     Return value : none
 ***************************************************************/
 void FPEC_voidEraseAllFlash();
+
+/*************************************************************
+    Function Name: FPEC_CheckForErrors
+    Description  : Check Error Flags in Status register
+    Parameters   : none
+    Return value : one of 3 values:
+                    WRITE_PROTECTION_ERROR
+                    PROGRAMMING_ERROR
+                    SUCCESSFULLL_OPERATION
+***************************************************************/
+u8 FPEC_CheckForErrors();
+
 
 #endif // End Of File
--- 02-MCAL/09- Flash Driver/FPEC_program.c
+++ 02-MCAL/09- Flash Driver/FPEC_program.c
@@ -1,9 +1,9 @@
-/********************************************************/ 
+/********************************************************/
 /* Author : Nourhan Mansour                             */
-/* Date   : 13/10/2020                                  */
-/* Version: 1.0                                         */
+/* Date   : 11/11/2020                                  */
+/* Version: 2.0                                         */
 /* File   : FPEC_program.c                              */
-/********************************************************/ 
+/********************************************************/
 
 // 1- include Libraries 
 #include "STD_TYPES.h"
@@ -16,88 +16,139 @@
 #include "FPEC_private.h"
 #include "FPEC_config.h"
 
+void FPEC_voidInit() {
+	// Unlock flash memory
 
-void FPEC_voidInit()
-{
-    // Unlock flash memory 
-
-    /************************** 
-    The key values are:
-        RDPRT key = 0x00A5
-        KEY1 = 0x45670123
-        KEY2 = 0xCDEF89AB
-    **************************/
-    FPEC -> KEYR = 0x45670123;
-    FPEC -> KEYR = 0xCDEF89AB;
+	/**************************
+	 The key values are:
+	 RDPRT key = 0x00A5
+	 KEY1 = 0x45670123
+	 KEY2 = 0xCDEF89AB
+	 **************************/
+	FPEC->KEYR = 0x45670123;
+	FPEC->KEYR = 0xCDEF89AB;
 
 }
 
-void FPEC_voidWriteData ( u16 * cpy_address , u16 * cpy_data , u8 cpy_length)
-{
-    u16 * dataPtr = cpy_data;
-    u8 i;
-    // data write (half word)
-    for(i=0 ; i< cpy_length ; i++)
-    {
-        // Set PG bit 
-        SET_BIT(FPEC->CR , FLASH_CR_PG);
-        // Write Data
-        *((volatile u16*)(cpy_address)) =  *dataPtr;
-        cpy_address++;
-        dataPtr++;
-        // wait BSY flag
-        while ( GET_BIT (FPEC->SR , FLASH_SR_BSY) == 1 );
-        /* EOP */
-		SET_BIT(FPEC->SR,FLASH_SR_EOP);
-        CLR_BIT(FPEC->CR,FLASH_CR_PER);
+void FPEC_UnlockOptionBytes(){
+	/**************************
+	 The key values are:
+	 RDPRT key = 0x00A5
+	 KEY1 = 0x45670123
+	 KEY2 = 0xCDEF89AB
+	 **************************/
+	FPEC->OPTKEYR = 0x45670123;
+	FPEC->OPTKEYR = 0xCDEF89AB;
 
-    }
-   
-    
 }
 
-void FPEC_voidEraseDataPage (u32 cpy_page)
+void FPEC_voidLock() {
+	/*
+	 When SET: FPEC and FLASH_CR are locked
+	 This bit is reset by hardware after
+	 detecting the unlock sequence.
+	 */
+	SET_BIT(FPEC -> CR, FLASH_CR_LOCK);
+}
+
+void FPEC_voidLockOptionBytes()
 {
+    /* Software Reset */
+    CLR_BIT(FPEC -> CR , FLASH_CR_OPTWRE);
+}
+
+u16 FPEC_u32ReadData(u16 * cpy_address) {
+	u16 val = *((volatile u16 *) cpy_address);
+	return val;
+
+}
+
+void FPEC_voidWriteData(u16 * cpy_address, u16 * cpy_data, u8 cpy_length) {
+	u16 * dataPtr = cpy_data;
+	u8 i;
+	// data write (half word)
+	// Set PG bit
+	SET_BIT(FPEC->CR, FLASH_CR_PG);
+	for (i = 0; i < cpy_length; i++) {
+		// Write Data
+		*((volatile u16*) (cpy_address)) = *dataPtr;
+		cpy_address++;
+		dataPtr++;
+		// wait BSY flag
+		while ( GET_BIT (FPEC->SR , FLASH_SR_BSY) == 1)
+			;
+		/* EOP */
+		SET_BIT(FPEC->SR, FLASH_SR_EOP);
+		CLR_BIT(FPEC->CR, FLASH_CR_PER);
+	}
+	// Disable Programming
+	CLR_BIT(FPEC->CR, FLASH_CR_PG);
+
+}
+
+void FPEC_voidEraseDataPage(u8 cpy_page) {
+	/* Check if FPEC is locked or not */
+	if ( GET_BIT(FPEC->CR,FLASH_CR_LOCK) == 1) {
+		FPEC->KEYR = 0x45670123;
+		FPEC->KEYR = 0xCDEF89AB;
+	}
 	/* Wait Busy Flag */
-	while (GET_BIT(FPEC->SR,FLASH_SR_BSY) == 1);
+	while (GET_BIT(FPEC->SR,FLASH_SR_BSY) == 1)
+		;
 
 	// SET PER bit
-    SET_BIT(FPEC->CR , FLASH_CR_PER);
+	SET_BIT(FPEC->CR, FLASH_CR_PER);
 
-    // select Page to erase in AR register 
-    FPEC->AR = (u32)(cpy_page * 1024) + 0x08000000 ;
+	// select Page to erase in AR register
+	FPEC->AR = (u32) (cpy_page * 1024) + 0x08000000;
 
-    // Set STRT bit 
-    SET_BIT(FPEC->CR , FLASH_CR_STRT);
-    
-    // Wait for BSY bit 
-    while ( GET_BIT (FPEC->SR , FLASH_SR_BSY) == 1 );
-    /* EOP */
-    SET_BIT(FPEC->SR,FLASH_SR_EOP);
-    CLR_BIT(FPEC->CR,FLASH_CR_PER);
-    
-}
+	// Set STRT bit
+	SET_BIT(FPEC->CR, FLASH_CR_STRT);
 
-void FPEC_voidEraseAllFlash()
-{
-    // Set the MER bit in the FLASH_CR register
-    SET_BIT (FPEC->CR , FLASH_CR_MER);
-    // Set the STRT bit in the FLASH_CR register
-    SET_BIT (FPEC->CR , FLASH_CR_STRT);
-    // Wait for the BSY bit to be reset
-    while ( GET_BIT (FPEC->SR , FLASH_SR_BSY) == 1 );
-    // Read all the pages and verify
-    SET_BIT(FPEC->SR,FLASH_SR_EOP);
-    CLR_BIT(FPEC->CR,FLASH_CR_PER);
+	// Wait for BSY bit
+	while ( GET_BIT (FPEC->SR , FLASH_SR_BSY) == 1)
+		;
+	/* EOP */
+	SET_BIT(FPEC->SR, FLASH_SR_EOP);
+	CLR_BIT(FPEC->CR, FLASH_CR_PER);
+
+	// Disable Erase
+	CLR_BIT(FPEC->CR, FLASH_CR_PER);
 
 }
 
-void FPEC_voidEraseRange(u8 cpy_startPage , u8 cpy_endPage)
+void FPEC_voidEraseAllFlash() {
+	// Set the MER bit in the FLASH_CR register
+	SET_BIT(FPEC->CR, FLASH_CR_MER);
+	// Set the STRT bit in the FLASH_CR register
+	SET_BIT(FPEC->CR, FLASH_CR_STRT);
+	// Wait for the BSY bit to be reset
+	while ( GET_BIT (FPEC->SR , FLASH_SR_BSY) == 1)
+		;
+	// Read all the pages and verify
+	SET_BIT(FPEC->SR, FLASH_SR_EOP);
+	CLR_BIT(FPEC->CR, FLASH_CR_PER);
+	// Disable Erase
+	CLR_BIT(FPEC->CR, FLASH_CR_PER);
+
+}
+
+void FPEC_voidEraseRange(u8 cpy_startPage, u8 cpy_endPage)
 {
-    if (cpy_endPage > cpy_startPage && cpy_endPage <= MAX_PAGE_NO){
-        u8 i ;
-        for(i=cpy_startPage ; i<=cpy_endPage ; i++){
-            FPEC_voidEraseDataPage(i);
-        }
-    }
+	if (cpy_endPage > cpy_startPage && cpy_endPage <= MAX_PAGE_NO) {
+		u8 i;
+		for (i = cpy_startPage; i <= cpy_endPage; i++) {
+			FPEC_voidEraseDataPage(i);
+		}
+	}
+}
+
+
+u8 FPEC_CheckForErrors()
+{
+    if(GET_BIT (FPEC ->  SR , FLASH_SR_WRPRTERR))
+        return WRITE_PROTECTION_ERROR;
+    if (GET_BIT (FPEC -> SR , FLASH_SR_PGERR))
+        return PROGRAMMING_ERROR;
+    return SUCCESSFULLL_OPERATION;
 }
